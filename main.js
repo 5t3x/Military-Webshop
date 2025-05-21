298590e70d5e7329445f8b855525a8048c07dd3b
Ghéczy Bálint <balintgheczy@gmail.com>
2025-05-21 11:30:02 +0200

    main

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 import Higlight from "./Highlight.js";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Kategoria from "./Kategoria.js";
 
 const kategoria = new Kategoria();
 kategoria.megjelenit();
 const higlight = new Higlight();
-=======
-// import  Kosar  from "./Kosar.js";
 
-const higlight = new Higlight();
-// const kosar =new Kosar();
->>>>>>> db78bc7f0d10755ca87091544367275c27a3b0a7
-=======
+
+
+
 import ScrollToTopButton from './Gomb.js';
-const higlight = new Higlight();
 const scrollToTopButton = new ScrollToTopButton();
->>>>>>> 666d42e936192e250b6f9c10bd585da4dc44cff2
+
