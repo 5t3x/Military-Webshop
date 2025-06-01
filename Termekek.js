8cfb0d8b42a1ed3bbdd858d43a460ca83d7c7bc4
GBalintG <balintgheczy@gmail.com>
2025-06-01 21:13:37 +0200

    BtnUp

--- Termekek.js
+++ Termekek.js
@@ -18,24 +18,3 @@ export default class Termekek {
         });
     }
 }
-
-// import Termek from "./Termek.js";
-// import { termekLista } from "./termekLista_2.js";
-
-// export default class Termekek {
-//     #szuloElem;
-
-//     constructor() {
-//         this.#szuloElem = document.querySelector(".tartalom");
-//         this.megjelenit();
-//     }
-
-//     megjelenit() {
-//         this.#szuloElem.innerHTML = `<div class="row"></div>`;
-//         const sorElem = this.#szuloElem.querySelector(".row");
-
-//         termekLista.forEach((termek, i) => {
-//             new Termek(termek, sorElem, i);
-//         });
-//     }
-// }
