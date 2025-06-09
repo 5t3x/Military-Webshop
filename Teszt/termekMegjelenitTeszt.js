a215882261f1bf1ba71d055bcef0c15a1ca6b300
GBalintG <balintgheczy@gmail.com>
2025-06-09 22:49:51 +0200

    Teszt-termekAdat javítása

--- Teszt/termekMegjelenitTeszt.js
+++ Teszt/termekMegjelenitTeszt.js
@@ -1,12 +1,12 @@
 import Termek from "../Termek.js";
 
 function termekMegjelenitTeszt() {
-
   const termekAdat = {
-    nev: "Taktikai hátizsák",
-    leiras: "50 literes, strapabíró, vízálló",
-    ar: 12000,
-    kategoria: "Felszerelés",
+    id: 1,
+    nev: "Katonai Taktikai Kabát",
+    ar: 10000,
+    leiras: "Strapabíró, vízálló kabát, 3 zsebbel",
+    kategoria: "Ruha fajták",
   };
 
   const tesztSzuloElem = document.createElement("div");
@@ -17,6 +17,5 @@ function termekMegjelenitTeszt() {
   const cardElem = tesztSzuloElem.querySelector(".card");
   console.assert(cardElem !== null, "A termek elem nem jött létre!");
   console.log("Sikeresen lefutott a termekMegjelenitTeszt!");
-
 }
 termekMegjelenitTeszt();
